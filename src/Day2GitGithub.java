0387e0f910e42748c5a4ab92071dc1d24ab58fc8
Mihrican Pürbudak <mihricanpurbudak5@gmail.com>
2022-08-15 20:40:54 +0200

    V3

---
+++ src/Day2GitGithub.java
@@ -0,0 +1,49 @@
+public class Day2GitGithub {
+    /*
+    1-) git init ---> Local repo olusturma icin yani .git ile
+    klasorumuzun icindeki dosyalari iliskilendirmek icin kullanilir
+    2-) git add .---> Working space den (yani yerel dosyalarimi staging area ya yani commitlemek icin eklenen yer)
+    gonderir
+    3-) git status ---> Working space ve staged area daki durumu gosterir
+    4-) git commit -m "mesaj" Staging area dan commit store a dosyalarimi gondermek icin kullanilir
+    (commit yani versiyon olusturmus olurum)
+    5-) git push ---> Uzak repoya( yani github a ) gondermek icin kullandigimiz kod
+    Yalniz git push komutunu direk kullanmak istersek 1 kez
+         git remote add origin adres
+         git push -u origin master
+     Not: Yukaridaki iki komutu tek seferde kullandiktan sonra ikinci commit lerim icin
+     sadece git push kullaniriz.
+     Not: git log --oneline -->commitlerimdeki yani versiyonlarimdaki durumu gosterir
+     */
+/*
+İlk defa PUSH yapacaksanız bir projeyi ;
+
+1 - Önce GitHub a gidip New Repository i oluşturun
+2- Oluşturduğunuz repository de size verilen https://github.com/.............git   adresini kopyalayın
+
+
+Şimdi IntelliJ ye gelip TERMINAL ı açın;
+
+3- git init yazın
+4- git add . yazın
+5- git commit -m"first commit"  (first commit yerine  ilke versiyon, ilk ders veya istediğiniz bir şeyi yazın)
+6- git branch -M main
+7- git remote add origin https://github.com/.............git (yukarıda2. satırda kopyalağınız adresi buraya yapıştırın)
+8- git push -u origin main
+
+
+SONRASINDA Aynı PROJE için ;
+
+her yeni eklediğiniz şeyden sonra GitHub'a yüklemek için
+
+9- git add .
+10 - git commit -m"YENI NUMARA VEYA YAZI YAZ"
+11 - git push
+
+
+hepsi bu kadar
+ */
+
+
+    }
+
