0b8d4ea23ae723d948766cc0511d12e98ead2c25
mihricanpurbudak <109859823+mihricanpurbudak@users.noreply.github.com>
2022-08-15 20:57:50 +0200

    Update Day2GitGithub.java

--- src/Day2GitGithub.java
+++ src/Day2GitGithub.java
@@ -14,6 +14,7 @@ public class Day2GitGithub {
      Not: Yukaridaki iki komutu tek seferde kullandiktan sonra ikinci commit lerim icin
      sadece git push kullaniriz.
      Not: git log --oneline -->commitlerimdeki yani versiyonlarimdaki durumu gosterir
+     Not:Github ogrendim
      */
 /*
 İlk defa PUSH yapacaksanız bir projeyi ;
